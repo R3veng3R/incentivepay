85e263b9ab7f627c03e74f82e9f4935604589480
Aleksei <evilstormhot@gmail.com>
2020-08-30 13:27:23 +0300

    Add payment endpoint Add axios, add proxy Add repository and model

---
+++ src/main/java/com/incentive/payment/service/PaymentRecordService.java
@@ -0,0 +1,30 @@
+package com.incentive.payment.service;
+
+import com.incentive.payment.model.PaymentRecord;
+import com.incentive.payment.repository.PaymentRecordRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+
+@Service
+public class PaymentRecordService {
+    private final Logger log = LoggerFactory.getLogger(PaymentRecordService.class);
+
+    private final PaymentRecordRepository paymentRecordRepository;
+
+    public PaymentRecordService(final PaymentRecordRepository paymentRecordRepository) {
+        this.paymentRecordRepository = paymentRecordRepository;
+    }
+
+    @Transactional
+    public PaymentRecord save(final PaymentRecord paymentRecord) {
+        if (paymentRecord.getCreated() == null) {
+            paymentRecord.setCreated(new Date());
+        }
+
+        return paymentRecordRepository.save(paymentRecord);
+    }
+}
